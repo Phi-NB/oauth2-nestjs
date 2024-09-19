e82a4579dcc687f107baac61642578464ad319be
PhiNB <nguyenbaphi2k1@gmail.com>
2024-09-19 10:30:59 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,8 +10,15 @@ import { OidcProviderModule } from './oidc/oidc-provider.module';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 3306),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       autoLoadEntities: true,
-      logging: true,
+      synchronize: true,
+      logging: process.env.NODE_ENV !== 'production',
     }),
     OauthModule,
     OidcProviderModule,
